5b99dae34e1e91e7ba7fb980978d82af7d8851cb
Lovro Predovan <lovro.predovan@gmail.com>
2017-05-25 08:02:34 +0200

    Added basic parsing lib

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	fmt.Println("Hello there curious stranger")
+	getItemContent()
+}
+
+// getCategoryContent is method that gets
+func getCategoryContent() {
+
+}
+
+// getItemContent is method that provides specific item info
+func getItemContent() {
+	doc, err := goquery.NewDocument("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		band := s.Find("a").Text()
+		title := s.Find("i").Text()
+		fmt.Printf("Review %d: %s - %s\n", i, band, title)
+	})
 }
