cceceb2a5cfb2923e7d6dccb38b342c93430d235
Lovro Predovan <lovro.predovan@gmail.com>
2017-05-28 11:50:57 +0200

    Added parsing regular and vau vau items

--- main.go
+++ main.go
@@ -7,41 +7,44 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+const baseURL string = "http://www.njuskalo.hr/"
+
 func main() {
-	//getItemContent()
-	getCategoryContent()
+	getLocation("iznajmljivanje-stanova", "zagreb")
 }
 
-// getCategoryContent is method that gets
-func getCategoryContent() {
-	//http://www.njuskalo.hr/iznajmljivanje-stanova/zagreb
+func getLocation(category string, param string) {
 
-	doc, err := goquery.NewDocument("http://www.njuskalo.hr/iznajmljivanje-stanova/zagreb")
+	doc, err := goquery.NewDocument(fmt.Sprintf("%s%s/%s", baseURL, category, param))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Find the review items
-	doc.Find(".EntityList-item article .entity-title").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
-		title := s.Find("a").Text()
-		fmt.Printf("Review %d: %s\n", i, title)
-	})
+	fmt.Println("Vau Vau offer")
+	getListContent(doc, ".EntityList--VauVau .EntityList-item article .entity-title")
 
+	fmt.Println("Regular offer")
+	getListContent(doc, ".EntityList--Standard .EntityList-item article .entity-title")
+}
+
+func getListContent(doc *goquery.Document, selector string) {
+
+	// Find the review items
+	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		titleElement := s.Find("a")
+
+		itemID, _ := titleElement.Attr("name")
+		itemTitle := titleElement.Text()
+
+		itemLink, _ := titleElement.Attr("href")
+		itemLink = fmt.Sprintf("%s%s", baseURL, itemLink)
+
+		fmt.Printf("Review %d: ID:%s - %s - %s\n", i+1, itemID, itemTitle, itemLink)
+	})
 }
 
 // getItemContent is method that provides specific item info
 func getItemContent() {
-	doc, err := goquery.NewDocument("http://metalsucks.net")
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	// Find the review items
-	doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
-		band := s.Find("a").Text()
-		title := s.Find("i").Text()
-		fmt.Printf("Review %d: %s - %s\n", i, band, title)
-	})
 }
