37912e15e2046db9c4bc907cda21f016c4c260fe
Lovro Predovan <lovro.predovan@gmail.com>
2017-05-28 10:56:45 +0200

    Added .gitignore and renamed tests file

--- main.go
+++ main.go
@@ -8,11 +8,25 @@ import (
 )
 
 func main() {
-	getItemContent()
+	//getItemContent()
+	getCategoryContent()
 }
 
 // getCategoryContent is method that gets
 func getCategoryContent() {
+	//http://www.njuskalo.hr/iznajmljivanje-stanova/zagreb
+
+	doc, err := goquery.NewDocument("http://www.njuskalo.hr/iznajmljivanje-stanova/zagreb")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	doc.Find(".EntityList-item article .entity-title").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		title := s.Find("a").Text()
+		fmt.Printf("Review %d: %s\n", i, title)
+	})
 
 }
 
